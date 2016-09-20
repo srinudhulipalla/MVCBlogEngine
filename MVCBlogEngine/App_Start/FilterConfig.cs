d009c3e60cb6431cee2c6ef408c64df153d7b5c2
srinudhulipalla <srinudhulipalla@gmail.com>
2016-09-20 09:27:08 +0530

    added

---
+++ MVCBlogEngine/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlogEngine
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
